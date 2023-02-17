f449b24b643162ba2d997a161427861b2b6150c1
ceechamisa <chamisa38@gmail.com>
2023-02-16 23:53:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,2 @@
-#include <studio.h>
-/**
- * main - Entry
- * Return: Always 0
- */
-int main(void)
-{
-	puts("\"Programing is like building a multilingual puzzle");
-	return (0);
-}
+#!/bin/bash
+gcc 4-puts.c -o 4-puts
